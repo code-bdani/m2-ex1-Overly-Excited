51662a851516bc10955a2da7fcdceccab972750b
Brittany D. Smith <bdaniphotography@gmail.com>
2018-02-27 21:20:57 -0600

    Got the sentence split into a new array of the words

--- overly-excited.js
+++ overly-excited.js
@@ -1,8 +1,10 @@
 // This program will loop over an array and gradually build a sentence incremented by one word on each new line.
 
-// Creates an array that contains the words in the sentence
-let sentence = ["The walrus danced through the trees in the light of the moon."];
-
+// Creates a variable containing a sentence in a single string
+let sentence = "The walrus danced through the trees in the light of the moon.";
+// Creates a new variable from the "sentence" variable with the ".split()" method applied, spliting the string into an array containing the individual words. The " " acts as a seperator.
+let theWordArray = sentence.split(" ");
+console.log(theWordArray);
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
