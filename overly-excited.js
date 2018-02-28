05c5f4c2a5f5a6115a9dc850c6ecfa59a7705a67
Brittany D. Smith <bdaniphotography@gmail.com>
2018-02-28 15:11:18 -0600

    Exclamation point is in the correct place!

--- overly-excited.js
+++ overly-excited.js
@@ -3,18 +3,19 @@
 // Creates a variable containing a sentence in a single string
 let sentence = "The walrus danced through the trees in the light of the moon.";
 
-// Redfines the value of the "sentence" variable with the ".split()" method applied, spliting the string into an array containing the individual words. The " " acts as a seperator.
+// Redfines the value of the "sentence" variable with the ".split()" method applied, spliting the string into an array containing the individual words. The " " acts as a deliminator.
 sentence = sentence.split(" ");
 
 // The addExcitement function should be an impure function, and accept the array as the sole argument.
 // It should iterate over the array and output the words to the browser console.
 function addExcitement (theWordArray) {
+    let space = "";
     for (let i = 0; i < theWordArray.length; i++) {
     let currentWordInArray = theWordArray[i];
-    sentence += " " + currentWordInArray;
-    console.log(sentence);
-        if (i % 3) {
-            sentence += "!";
+    space += " " + currentWordInArray;
+    console.log(space);
+        if ((i + 1) % 3 === 0) {
+            space += "!";
         }
     }
 }
